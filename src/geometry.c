6b2dec726b2cfb7f817bb6f4eb6dcfe6d2fc2e6a
OodizzapandaoO <gh0st1990@mail.ru>
2019-05-27 18:07:07 +0700

    add geometry

---
+++ src/geometry.c
@@ -0,0 +1,11 @@
+#include <math.h>
+#include <stdio.h>
+int main() {
+  float x1, x2, y1, y2, r1, r2, p1, p2, s1, s2, ras1, ras2;
+  printf("Радиусы окружностей: \n");
+  scanf("%f %f", &r1, &r2);
+  printf("Координаты x1 y1 x2 y2: \n");
+  scanf("%f %f %f %f", &x1, &y1, &x2, &y2);
+
+  return 0;
+}
